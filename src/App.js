4495f390b63d492dbf5b08583172a032a6fe1a76
Maurikzio <m.morocho1196@gmail.com>
2020-08-02 00:35:59 +0300

    [add] initial view of the app with dummy code so far.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import './App.css';
 
 function App() {
+  const [ input, setInput ] = useState('');
+  const [ messages, setMessages ] = useState([]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput('')
+  }
+
   return (
     <div className="App">
-      <center>Initializing Facebook messenger app</center>
+      <center>
+      <FormControl>
+        <InputLabel >Email address</InputLabel>
+        <Input value={input} onChange={(e) => setInput(e.target.value)}/>
+        <Button disabled={!input} variant="contained" color='primary'>Send message</Button>
+      </FormControl>
+      </center>
+
+      {
+        messages.map((message, ind) => (
+          <p key={ind}>{message}</p>
+        ))
+      }
     </div>
   );
 }
